f4ab8f0e6a4e3f662707a6862bb905e0fed45ffa
Mario Mitchell <mario@undergroundfx.com>
2018-11-16 00:54:21 -0800

    Classes Drill

---
+++ ClassesDrill/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill
+{
+    public class Math
+    {
+        // all classes must have this constructor
+        public Math()
+        {
+
+        }
+
+        // create an int valued method
+        public int Addition(int num)
+        {
+            int answer = num + 42;
+            return answer;
+        }
+
+        public int Multiply(int num)
+        {
+            int answer = num * 42;
+            return answer;
+        }
+
+        public int Divide(int num)
+        {
+            int answer = num / 42;
+            return answer;
+        }
+    }
+}
