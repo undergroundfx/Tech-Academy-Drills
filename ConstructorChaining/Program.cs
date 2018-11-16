5528acba0e0a952144e27309fbb51e9e836c2146
Mario Mitchell <mario@undergroundfx.com>
2018-11-16 01:54:45 -0800

    Add files via upload

---
+++ ConstructorChaining/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConstructorChaining
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string title = "The Wizard of Oz";
+            var rating = " receives 4 Stars";
+            Console.WriteLine(title + rating);
+            
+            Console.WriteLine(Managers.funcTest());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Workers
+    {
+        public Workers(string name) : this(name, 100)
+        {
+
+        }
+
+        public Workers(string name, int test) 
+        {
+            string names = "Mario";
+            name = names;
+        }
+
+    }
+
+    class Managers
+    {
+        Managers()
+        {
+
+        }
+
+        public static string funcTest()
+        {
+            return "42";
+        }
+
+    }
+
+}
---
+++ DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please Enter a Number: ");   
+
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(DateTime.Now.AddHours(num));
+            Console.ReadLine();
+        }
+    }
+}
